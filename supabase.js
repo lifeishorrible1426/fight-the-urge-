66c059b04f135a2e045a2116fe4aa80acaffa721
lifeishorrible1426 <fmnddd@proton.me>
2026-08-08 18:39:18 -0400

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = https://vzsqijgsuqazeawvfmih.supabase.co/rest/v1/;
+const SUPABASE_PUBLISHABLE_KEY =sb_publishable_Asby2cq5HiRgffAHoRR4tA_yz87ZCEX ;
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_PUBLISHABLE_KEY
+);
